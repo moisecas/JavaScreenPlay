695445a0b7f12fa9de8cdb2bdc0c47809a9481ad
mcastro <mcastro@talana.com>
2024-09-18 10:56:06 -0500

    modificación de ruta

--- src/test/java/co/com/screenplay/project/runners/DemoBlazeRunner.java
+++ src/test/java/co/com/screenplay/project/runners/DemoBlazeRunner.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
-        features = "src/test/resources/features/demoblaze.feature",
+        features = "src/test/resources/features/navigation.feature",
         glue = "co.com.screenplay.project.stepdefinition"
 )
 
