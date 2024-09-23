14c310bd2b3dcc3df10280dcddc7845f5edf7c4c
mcastro <mcastro@talana.com>
2024-09-23 15:04:05 -0500

    mejorando metodos

--- src/main/java/co/com/screenplay/project/questions/ProductListVisible.java
+++ src/main/java/co/com/screenplay/project/questions/ProductListVisible.java
@@ -4,12 +4,12 @@ import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.questions.Visibility;
 
-import static co.com.screenplay.project.ui.DemoBlazeHomePage.CATEGORY_PHONES;
+import static co.com.screenplay.project.ui.DemoBlazeHomePage.LIST_LAPTOPS;
 
 public class ProductListVisible implements Question<Boolean> {
     @Override
     public Boolean answeredBy(Actor actor) {
-        return Visibility.of(CATEGORY_PHONES).answeredBy(actor).booleanValue();
+        return Visibility.of(LIST_LAPTOPS).answeredBy(actor).booleanValue();
     }
 
     public static ProductListVisible isDisplayed() {
--- src/main/java/co/com/screenplay/project/ui/DemoBlazeHomePage.java
+++ src/main/java/co/com/screenplay/project/ui/DemoBlazeHomePage.java
@@ -6,6 +6,6 @@ import org.openqa.selenium.By;
 public class DemoBlazeHomePage {
     public static final Target CATEGORY_LAPTOPS = Target.the("Categoría Laptops")
             .located(By.xpath("//a[text()='Laptops']"));
-    public static final Target CATEGORY_PHONES = Target.the("Lista de productos")
-            .located(By.xpath("//div[@id='contcont']//a[2]"));
+    public static final Target LIST_LAPTOPS = Target.the("Lista de productos")
+            .located(By.xpath("//a[normalize-space()='Sony vaio i5']"));
 }
